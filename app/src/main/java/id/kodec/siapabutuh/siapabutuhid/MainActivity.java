ad27dd7937e3176b193ef2cd444c83843a387159
Ilhamsat <ilhamsatriadi10@gmail.com>
2018-11-08 12:45:59 +0700

    Beda Slider

--- app/src/main/java/id/kodec/siapabutuh/siapabutuhid/MainActivity.java
+++ app/src/main/java/id/kodec/siapabutuh/siapabutuhid/MainActivity.java
@@ -1,22 +1,37 @@
 package id.kodec.siapabutuh.siapabutuhid;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.kodec.siapabutuh.siapabutuhid._sliders.FragmentSlider;
+import id.kodec.siapabutuh.siapabutuhid._sliders.SliderIndicator;
+import id.kodec.siapabutuh.siapabutuhid._sliders.SliderPagerAdapter;
+import id.kodec.siapabutuh.siapabutuhid._sliders.SliderView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SliderPagerAdapter mAdapter;
+    private SliderIndicator mIndicator;
+    private SliderView sliderView;
+    private LinearLayout mLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
-        viewPager.setAdapter(viewPagerAdapter);
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+//        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+//        viewPager.setAdapter(viewPagerAdapter);
 
         ImageButton buttonMakanan = (ImageButton) findViewById(R.id.buttonMakanan);
         buttonMakanan.setOnClickListener(new View.OnClickListener() {
@@ -72,5 +87,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        sliderView = (SliderView) findViewById(R.id.sliderView);
+        mLinearLayout = (LinearLayout) findViewById(R.id.pagesContainer);
+        setupSlider();
+
     }
+
+    private void setupSlider() {
+        sliderView.setDurationScroll(800);
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-1.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-2.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-3.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-4.jpg"));
+
+        mAdapter = new SliderPagerAdapter(getSupportFragmentManager(), fragments);
+        sliderView.setAdapter(mAdapter);
+        mIndicator = new SliderIndicator(this, mLinearLayout, sliderView, R.drawable.indicator_circle);
+        mIndicator.setPageCount(fragments.size());
+        mIndicator.show();
+    }
+
 }
